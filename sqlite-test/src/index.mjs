9e099458a4398ee3f432a9d32f4c3577f6971f9f
刘鹏 <2254842466@qq.com>
2025-01-17 11:30:03 +0800

    sqlite-test

---
+++ sqlite-test/src/index.mjs
@@ -0,0 +1,22 @@
+import { DatabaseSync } from 'node:sqlite';
+// const database = new DatabaseSync('data.db');
+const database = new DatabaseSync(':memory:'); // 内存数据库
+
+
+database.exec(`
+  CREATE TABLE student(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INT
+  ) STRICT
+`);
+
+const insert = database.prepare('INSERT INTO student (id, name, age) VALUES (?, ?, ?)');
+insert.run(1, '张三', 20);
+insert.run(2, '李四', 21);
+insert.run(3, '王五', 22);
+
+const query = database.prepare('SELECT * FROM student ORDER BY id');
+console.log(query.all());
+
+// node --experimental-sqlite ./src/index.mjs
